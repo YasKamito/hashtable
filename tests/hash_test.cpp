14286ae4dc1c4922f10ed1bede7ef7b0e48b3f4f
Yasuyuki Kamitokusari <EB21246@jp.ibm.com>
2015-07-02 15:55:50 +0900

    add test case
    	modified:   tests/hash_test.cpp

--- tests/hash_test.cpp
+++ tests/hash_test.cpp
@@ -16,25 +16,30 @@ extern "C"
 #include "gtest/gtest.h" /** GoogleTest インクルード */
 
 
-int mock_getRandam(int a, int b){
+int mock_hash(char *pchStr){
     return 5;
 }
-nodeptr hashtable[HASHSIZE];
 
+nodeptr hashtable[HASHSIZE];
 /*******************************************************************************
 * テストクラスの定義
 *******************************************************************************/
 class HashTest : public ::testing::Test
 {
 
+	int (*saved_hash)(char *);
+
   virtual void SetUp()
   {
 
+	  saved_hash = hash;
 
   }
 
   virtual void TearDown()
   {
+
+	  hash = saved_hash;
 
   }
 };
@@ -47,7 +52,16 @@ class HashTest : public ::testing::Test
 *******************************************************************************/
 TEST_F(HashTest, CASE001)
 {
-    EXPECT_EQ(0, hash_init(hashtable) );
+	int i;
+
+    EXPECT_EQ(0, hash_init(hashtable, 0) );
+
+	for(i = 0; i < HASHSIZE; i++){
+		EXPECT_TRUE((hashtable[i])->left == 0x00);
+		EXPECT_TRUE((hashtable[i])->right == 0x00);
+		EXPECT_TRUE((hashtable[i])->key != 0x00);
+		EXPECT_EQ(0, (hashtable[i])->val );
+	}
 }
 
 
@@ -61,13 +75,24 @@ TEST_F(HashTest, CASE002)
 {
 	char key[10], *pkey;
 	pkey = key;
-	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "aaaa");
-    EXPECT_EQ(1, hash_insert(hashtable,pkey,1) );
+
+	struct node outnode;
+	nodeptr pout;
+	pout = &outnode;
+
+    EXPECT_EQ(0, hash_init(hashtable, 0) );
 
 	memset(pkey,0x00,sizeof(key));
 	strcpy(key, "aaaa");
-    EXPECT_EQ(0, hash_insert(hashtable,pkey,1) );
+    EXPECT_EQ(1, hash_insert(hashtable,pkey,1, &pout) );
+    EXPECT_EQ(1, (pout)->val );
+
+	memset(pkey,0x00,sizeof(key));
+	strcpy(key, "aaaa");
+    EXPECT_EQ(0, hash_insert(hashtable,pkey,1, &pout) );
+    EXPECT_EQ(1, (pout)->val );
+
+    EXPECT_EQ(0, hash_terminate(hashtable) );
 }
 
 
@@ -81,86 +106,26 @@ TEST_F(HashTest, CASE003)
 {
 	char key[10], *pkey;
 	pkey = key;
-	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "bbbb");
-    EXPECT_EQ(1, hash_insert(hashtable,pkey,2) );
 
-	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "cccc");
-    EXPECT_EQ(1, hash_insert(hashtable,pkey,3) );
-
-	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "dddd");
-    EXPECT_EQ(1, hash_insert(hashtable,pkey,4) );
-
-	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "dddd");
-    EXPECT_EQ(0, hash_insert(hashtable,pkey,4) );
-}
-
-/*******************************************************************************
-* テストケース１
-*
-* 条件：
-*
-*******************************************************************************/
-TEST_F(HashTest, CASE004)
-{
-    EXPECT_EQ(0, hash_terminate(hashtable) );
-
-	char key[10], *pkey;
-	pkey = key;
-	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "dddd");
-    EXPECT_EQ(1, hash_insert(hashtable,pkey,2) );
-
-	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "cccc");
-    EXPECT_EQ(1, hash_insert(hashtable,pkey,2) );
-
-    EXPECT_EQ(0, hash_terminate(hashtable) );
-}
-
-
-/*******************************************************************************
-* テストケース１
-*
-* 条件：
-*
-*******************************************************************************/
-TEST_F(HashTest, CASE005)
-{
-	char key[10], *pkey;
-	pkey = key;
-
-	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "dddd");
-    EXPECT_EQ(1, hash_insert(hashtable,pkey,4) );
-
-	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "cccc");
-    EXPECT_EQ(1, hash_insert(hashtable,pkey,3) );
+	struct node outnode;
+	nodeptr pout;
+	pout = &outnode;
 
 	memset(pkey,0x00,sizeof(key));
 	strcpy(key, "bbbb");
-    EXPECT_EQ(1, hash_insert(hashtable,pkey,2) );
-
-	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "dddd");
-    EXPECT_EQ(0, hash_insert(hashtable,pkey,4) );
+    EXPECT_EQ(1, hash_insert(hashtable,pkey,2, &pout) );
 
 	memset(pkey,0x00,sizeof(key));
 	strcpy(key, "cccc");
-    EXPECT_EQ(0, hash_insert(hashtable,pkey,4) );
+    EXPECT_EQ(1, hash_insert(hashtable,pkey,3, &pout) );
 
 	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "aaaa");
-    EXPECT_EQ(1, hash_insert(hashtable,pkey,1) );
+	strcpy(key, "dddd");
+    EXPECT_EQ(1, hash_insert(hashtable,pkey,4, &pout) );
 
 	memset(pkey,0x00,sizeof(key));
-	strcpy(key, "eeee");
-    EXPECT_EQ(1, hash_insert(hashtable,pkey,2) );
+	strcpy(key, "dddd");
+    EXPECT_EQ(0, hash_insert(hashtable,pkey,4, &pout) );
 
-
-
+    EXPECT_EQ(0, hash_terminate(hashtable) );
 }
